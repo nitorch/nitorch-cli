f655023deb4a84034e7140d9af571fbb7b6975f6
Yael Balbastre <yael.balbastre@gmail.com>
2023-05-16 18:42:26 -0400

    ENH: use commands registry

--- nitorch_cli/entrypoint.py
+++ nitorch_cli/entrypoint.py
@@ -1,18 +1,8 @@
-"""Implementation of the command-line entry point for nitorch's tools.
-
-Subcommands should register themselves by doing:
-```python
->> from nitorch.tools.cli import commands
->> commands['commandname'] = command_fn
-```
-The registered function should be a parser which takes the list of
-command-line arguments (without the command name) as input.
-"""
+"""Implementation of the command-line entry point for nitorch tools."""
 import sys
-
-
-# Subcommands should registers themselves in this dictionary
-commands = dict()
+import yaml
+import importlib
+import os.path
 
 
 _help = r"""[nitorch] Command-line tools
@@ -24,12 +14,12 @@ usage:
 """
 
 
-def help():
+def help(registry):
     """Help for the generic 'nitorch' command.
     List all registered commands.
     """
 
-    commandnames = sorted(commands.keys())
+    commandnames = sorted(registry.keys())
     length = max(len(name) for name in commandnames)
     nb_col = 76//(length+1)
     commandlist = "    "
@@ -48,27 +38,57 @@ def nitorch(args=None):
     """
 
     args = args or sys.argv[1:]  # remove command name
+    registry = parse_registry()
 
     if not args:
-        print(help())
+        print(help(registry))
         return
     tag, *args = args
     if tag == 'help':
         if args:
-            if args[0] in commands.keys():
-                return commands[args[0]](['-h'])
+            if args[0] in registry.keys():
+                command = load_function(registry[args[0]])
+                return command(['-h'])
             else:
                 print(help)
                 print(f'[ERROR] Unknown command "{args[0]}"', file=sys.stderr)
                 return 1
         else:
-            print(help())
+            print(help(registry))
             return
     if tag in ('-h', '--help'):
-        print(help())
+        print(help(registry))
         return
-    if tag not in commands.keys():
-        print(help())
+    if tag not in registry.keys():
+        print(help(registry))
         print(f'[ERROR] Unknown command "{tag}"', file=sys.stderr)
         return 1
-    return commands[tag](args)
+    command = load_function(registry[args[0]])
+    return command(args)
+
+
+def parse_registry():
+    this_folder = os.path.dirname(__file__)
+    path_registry = os.path.join(this_folder, 'registry.yml')
+    if not os.path.exists(path_registry):
+        raise ValueError('Cannot find registry')
+    with open(path_registry, 'r') as file:
+        registry_yml = yaml.safe_load(file)
+
+    registry = {}
+    for module, commands in registry_yml.items():
+        for command in commands:
+            if isinstance(command, dict):
+                command, path_to_entrypoint = next(command.items().__iter__())
+            else:
+                path_to_entrypoint = f'cli.{command}.entrypoint'
+            path_to_entrypoint = module + '.' + path_to_entrypoint
+            registry[command] = path_to_entrypoint
+    return registry
+
+
+def load_function(path):
+    *path, function = path.split('.')
+    path = '.'.join(path)
+    module = importlib.import_module(path)
+    return getattr(module, function)
